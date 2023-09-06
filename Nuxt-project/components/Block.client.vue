2894d36b245014f3da352ab30bfde66dc3671010
Firdavs <mirzozodafirdavs837@gmail.com>
2023-09-06 09:42:40 -0700

    url

--- Nuxt-project/components/Block.client.vue
+++ Nuxt-project/components/Block.client.vue
@@ -13,7 +13,7 @@ const moreThen = ref(false)
     const cityName = ref('')
 
     async function selectCity(){
-    const cityUrl = fetch(`http://192.168.0.114:8000/api/city`)
+    const cityUrl = fetch(`http://127.0.0.1:8000/api/city`)
     const c = await cityUrl;
     const city = await c.json()
     showCities.value = city
